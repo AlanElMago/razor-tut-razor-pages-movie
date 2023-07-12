f1fa98b78c22b2660dae7b3a40346c149d569aa5
Alan Franco <alanfrancob97@gmail.com>
2023-07-11 18:28:32 -0600

    Added data annotations to Movie entity

---
+++ Migrations/20230712002549_New_DataAnnotations.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace razor_tut_razor_pages_movie.Migrations
+{
+    public partial class New_DataAnnotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "TEXT",
+                maxLength: 5,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18, 2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 5);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "decimal(18, 2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,17 +7,19 @@ namespace RazorPagesMovie.Models
     {
         public int ID { get; set; }
 
+        [Required, StringLength(60, MinimumLength = 3)]
         public string Title { get; set; } = string.Empty;
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [DataType(DataType.Date), Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
+        [Required, StringLength(30), RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string Genre { get; set; } = string.Empty;
 
-        [Column(TypeName = "decimal(18, 2)")]
+        [DataType(DataType.Currency), Column(TypeName = "decimal(18,2)"), Range(1, 100)]
         public decimal Price { get; set; }
 
+        [Required, StringLength(5), RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
         public string? Rating { get; set; } = string.Empty;
     }
 }
